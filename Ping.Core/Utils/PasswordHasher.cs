e6d32d50935b6f6569ce4eabc7df82a9e46f896f
Grant Burke <grant.s.burke@gmail.com>
2023-04-19 21:05:06 -0500

    Added verify method to password hasher

--- Ping.Core/Utils/PasswordHasher.cs
+++ Ping.Core/Utils/PasswordHasher.cs
@@ -4,4 +4,7 @@ public static class PasswordHasher
 {
     public static string HashPassword(string plainTextPassword)
         => BCrypt.Net.BCrypt.HashPassword(plainTextPassword);
+
+    public static bool VerifyPassword(string plainTextPassword, string hashedPassword)
+        => BCrypt.Net.BCrypt.Verify(plainTextPassword, hashedPassword);
 }
