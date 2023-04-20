9a4e2962f1679d9800cff59475355eade01964d8
Grant Burke <grant.s.burke@gmail.com>
2023-04-19 23:01:28 -0500

    WIP: Adding Layout view

--- Ping.Web/Client/src/Shared/Layout.svelte
+++ Ping.Web/Client/src/Shared/Layout.svelte
@@ -1,30 +1,247 @@
 <script>
-	import svelteLogo from '../assets/svelte.svg'
-	import viteLogo from '/vite.svg'
+	import { inertia, page } from '@inertiajs/svelte'
+
+	const links = [
+		{ name: 'Dashboard', url: '/', componentName: 'Dashboard/Index' },
+		{
+			name: 'Organizations',
+			url: '/organizations',
+			componentName: 'Organizations/Index',
+		},
+		{ name: 'Contacts', url: '/contacts', componentName: 'Contacts/Index' },
+		{ name: 'Reports', url: '/reports', componentName: 'Reports/Index' },
+	]
+
+	let show_menu = false
+	function handle_menu_button_click() {
+		show_menu = !show_menu
+	}
 </script>
 
-<main>
-	<div>
-		<a href="https://vitejs.dev" target="_blank" rel="noreferrer">
-			<img src={viteLogo} class="logo" alt="Vite Logo" />
-		</a>
-		<a href="https://svelte.dev" target="_blank" rel="noreferrer">
-			<img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-		</a>
-	</div>
-	<h1>Vite + Svelte</h1>
+<div class="min-h-full">
+	<nav class="bg-gray-800">
+		<div class="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+			<div class="flex h-16 items-center justify-between">
+				<div class="flex items-center">
+					<div class="flex-shrink-0">
+						<svg
+							viewBox="0 0 1185 266"
+							class="w-32 text-white"
+							xmlns="http://www.w3.org/2000/svg"
+							fill="currentColor"
+							><path
+								d="M77.463 265c-19.497 0-35.318-15.405-35.318-34.39v-22.054C17.987 202.676 0 181.326 0 155.948V55.206C0 25.291 24.946 1 55.668 1h154.664C241.054 1 266 25.29 266 55.206v100.806c0 29.916-24.946 54.206-55.668 54.206H145.67c-2.823 0-5.383 1.407-6.827 3.58-10.7 17.067-24.158 31.897-39.98 43.915-6.236 4.794-13.654 7.287-21.4 7.287zM55.701 27.336c-15.771 0-28.65 12.465-28.65 27.87v100.806c0 15.342 12.813 27.87 28.65 27.87 7.49 0 13.536 5.881 13.536 13.168v33.624c0 4.922 4.272 7.99 8.214 7.99 1.709 0 3.286-.575 4.732-1.662 13.273-10.1 24.576-22.565 33.578-36.947 6.309-10.036 17.743-16.237 29.965-16.237h64.727c15.77 0 28.65-12.464 28.65-27.87V55.206c0-15.341-12.814-27.87-28.65-27.87H55.7z" /><path
+								d="M395.752 2.4c37.152 0 65.088 27.936 65.088 64.8 0 36.576-27.936 64.8-65.088 64.8h-46.368v72H322.6V2.4h73.152zm0 104.544c22.176 0 38.592-16.992 38.592-39.744 0-23.04-16.416-39.744-38.592-39.744h-46.368v79.488h46.368zM502.6 33.792c-9.504 0-16.992-7.488-16.992-16.704 0-9.216 7.488-16.992 16.992-16.992 9.216 0 16.704 7.776 16.704 16.992 0 9.216-7.488 16.704-16.704 16.704zM489.928 204V60h25.056v144h-25.056zM625 56.256c33.696 0 55.872 22.464 55.872 59.328V204h-25.056v-86.976c0-23.616-13.536-36.864-35.712-36.864-23.04 0-41.76 13.536-41.76 47.52V204h-25.056V60h25.056v20.736C589 63.744 604.84 56.256 625 56.256zM835.24 60h24.768v137.952c0 44.928-36 67.392-73.44 67.392-32.256 0-56.448-12.384-68.256-35.136l21.888-12.384c6.624 13.536 18.72 24.192 46.944 24.192 29.952 0 48.096-16.992 48.096-44.064v-20.448c-11.52 17.568-29.952 28.8-54.144 28.8-40.896 0-73.44-33.12-73.44-75.168 0-41.76 32.544-74.88 73.44-74.88 24.192 0 42.624 10.944 54.144 28.512V60zm-51.264 122.4c29.088 0 51.264-22.176 51.264-51.264 0-28.8-22.176-50.976-51.264-50.976-29.088 0-51.264 22.176-51.264 50.976 0 29.088 22.176 51.264 51.264 51.264zM946.8 205.08c-28.21 0-45.63-20.8-41.08-48.88 4.42-27.17 26.91-46.28 53.56-46.28 19.37 0 31.59 9.36 38.35 22.36l-23.79 12.61c-3.25-5.85-9.1-9.49-16.9-9.49-12.35 0-23.14 9.23-25.35 22.1-2.08 11.83 4.29 22.1 17.16 22.1 8.06 0 13.91-4.03 18.59-10.14l21.58 13.65c-9.36 13.78-24.44 21.97-42.12 21.97zm126.36-59.93c-1.95 11.18-8.58 19.5-18.2 24.44l11.7 33.28h-26l-9.36-28.6h-8.32l-5.07 28.6h-26l16.12-91h36.4c18.33 0 32.24 13.65 28.73 33.28zm-43.42-9.36l-2.99 16.9h10.66c5.07.13 8.84-2.99 9.75-8.32.91-5.33-1.82-8.58-7.02-8.58h-10.4zM1184.05 112l-15.99 91h-26l7.67-43.81-25.48 33.54h-2.34l-14.82-35.23-7.93 45.5h-26l15.99-91h26l13.65 37.31 27.95-37.31h27.3z" /></svg>
+					</div>
+					<div class="hidden md:block">
+						<div class="ml-10 flex items-baseline space-x-4">
+							<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+							{#each links as link}
+								{@const active =
+									link.componentName === $page.component ||
+									($page.url.startsWith(link.url) &&
+										link.url.length > 1)}
+								<a
+									href={link.url}
+									use:inertia
+									class={`rounded-md px-3 py-2 text-sm font-medium ${
+										active
+											? 'bg-gray-900 text-white'
+											: 'text-gray-300 hover:bg-gray-700 hover:text-white'
+									}`}>
+									{link.name}
+								</a>
+							{/each}
+						</div>
+					</div>
+				</div>
+				<div class="hidden md:block">
+					<div class="ml-4 flex items-center md:ml-6">
+						<!-- Profile dropdown -->
+						<div class="relative ml-3">
+							<div>
+								<button
+									type="button"
+									class="flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+									on:click={handle_menu_button_click}
+									id="user-menu-button"
+									aria-expanded="false"
+									aria-haspopup="true">
+									<span class="sr-only">Open user menu</span>
+									<img
+										class="h-8 w-8 rounded-full"
+										src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+										alt="" />
+								</button>
+							</div>
 
-	<div class="card">
-		<slot />
-	</div>
+							<!--
+                Dropdown menu, show/hide based on menu state.
 
-	<p>
-		Check out <a
-			href="https://github.com/sveltejs/kit#readme"
-			target="_blank"
-			rel="noreferrer">SvelteKit</a
-		>, the official Svelte app framework powered by Vite!
-	</p>
+                Entering: "transition ease-out duration-100"
+                  From: "transform opacity-0 scale-95"
+                  To: "transform opacity-100 scale-100"
+                Leaving: "transition ease-in duration-75"
+                  From: "transform opacity-100 scale-100"
+                  To: "transform opacity-0 scale-95"
+              -->
+							<div
+								class="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
+								class:hidden={!show_menu}
+								role="menu"
+								aria-orientation="vertical"
+								aria-labelledby="user-menu-button"
+								tabindex="-1">
+								<!-- Active: "bg-gray-100", Not Active: "" -->
+								<a
+									href="#"
+									class="block px-4 py-2 text-sm text-gray-700"
+									role="menuitem"
+									tabindex="-1"
+									id="user-menu-item-0">Your Profile</a>
+								<a
+									href="#"
+									class="block px-4 py-2 text-sm text-gray-700"
+									role="menuitem"
+									tabindex="-1"
+									id="user-menu-item-1">Settings</a>
+								<a
+									href="#"
+									class="block px-4 py-2 text-sm text-gray-700"
+									role="menuitem"
+									tabindex="-1"
+									id="user-menu-item-2">Sign out</a>
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="-mr-2 flex md:hidden">
+					<!-- Mobile menu button -->
+					<button
+						type="button"
+						class="inline-flex items-center justify-center rounded-md bg-gray-800 p-2 text-gray-400 hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
+						on:click={handle_menu_button_click}
+						aria-controls="mobile-menu"
+						aria-expanded="false">
+						<span class="sr-only">Open main menu</span>
+						<!-- Menu open: "hidden", Menu closed: "block" -->
+						<svg
+							class="block h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+						</svg>
+						<!-- Menu open: "block", Menu closed: "hidden" -->
+						<svg
+							class="hidden h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M6 18L18 6M6 6l12 12" />
+						</svg>
+					</button>
+				</div>
+			</div>
+		</div>
 
-	<p class="read-the-docs">Click on the Vite and Svelte logos to learn more</p>
-</main>
+		<!-- Mobile menu, show/hide based on menu state. -->
+		<div class="md:hidden" class:hidden={!show_menu} id="mobile-menu">
+			<div class="space-y-1 px-2 pb-3 pt-2 sm:px-3">
+				<!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
+				<a
+					href="#"
+					class="block rounded-md bg-gray-900 px-3 py-2 text-base font-medium text-white"
+					aria-current="page">Dashboard</a>
+				<a
+					href="#"
+					class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+					>Team</a>
+				<a
+					href="#"
+					class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+					>Projects</a>
+				<a
+					href="#"
+					class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+					>Calendar</a>
+				<a
+					href="#"
+					class="block rounded-md px-3 py-2 text-base font-medium text-gray-300 hover:bg-gray-700 hover:text-white"
+					>Reports</a>
+			</div>
+			<div class="border-t border-gray-700 pb-3 pt-4">
+				<div class="flex items-center px-5">
+					<div class="flex-shrink-0">
+						<img
+							class="h-10 w-10 rounded-full"
+							src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+							alt="" />
+					</div>
+					<div class="ml-3">
+						<div class="text-base font-medium leading-none text-white">
+							Tom Cook
+						</div>
+						<div class="text-sm font-medium leading-none text-gray-400">
+							tom@example.com
+						</div>
+					</div>
+					<button
+						type="button"
+						class="ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+						<span class="sr-only">View notifications</span>
+						<svg
+							class="h-6 w-6"
+							fill="none"
+							viewBox="0 0 24 24"
+							stroke-width="1.5"
+							stroke="currentColor"
+							aria-hidden="true">
+							<path
+								stroke-linecap="round"
+								stroke-linejoin="round"
+								d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0" />
+						</svg>
+					</button>
+				</div>
+				<div class="mt-3 space-y-1 px-2">
+					<a
+						href="#"
+						class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+						>Your Profile</a>
+					<a
+						href="#"
+						class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+						>Settings</a>
+					<a
+						href="#"
+						class="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
+						>Sign out</a>
+				</div>
+			</div>
+		</div>
+	</nav>
+
+	<header class="bg-white shadow">
+		<div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
+			<h1 class="text-3xl font-bold tracking-tight text-gray-900">
+				Dashboard
+			</h1>
+		</div>
+	</header>
+	<main>
+		<div class="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8">
+			<slot />
+		</div>
+	</main>
+</div>
--- Ping.Web/Client/tailwind.config.cjs
+++ Ping.Web/Client/tailwind.config.cjs
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ['./src/app.css', './src/**/*.{svelte,js}'],
+	content: ['../Views/**/*.cshtml', './src/app.css', './src/**/*.{svelte,js}'],
 	darkMode: 'class',
 	theme: {
 		extend: {},
