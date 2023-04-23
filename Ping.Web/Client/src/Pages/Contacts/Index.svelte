01533622fa5581b738aa127ae42ab6667dd61663
Grant Burke <grant.s.burke@gmail.com>
2023-04-23 15:41:19 -0500

    Added contacts page

--- Ping.Web/Client/src/Pages/Contacts/Index.svelte
+++ Ping.Web/Client/src/Pages/Contacts/Index.svelte
@@ -1,7 +1,64 @@
 <script>
+	import { inertia, page, router } from '@inertiajs/svelte'
 	import PageLayout from '../../Shared/PageLayout.svelte'
+	import Pagination from '../../Shared/Pagination.svelte'
+	import SearchFilter from '../../Shared/SearchFilter.svelte'
+
+	const route = 'contacts'
+
+	$: ({
+		data: contacts,
+		page: currentPage,
+		perPage,
+		total,
+		pageNumbers,
+		previousButtonDisabled,
+		nextButtonDisabled,
+	} = $page.props)
 </script>
 
 <PageLayout title="Contacts">
-	<p class="bg-green-500 text-white">Contacts</p>
+	<div class="flex items-center justify-between">
+		<SearchFilter {route} />
+		<a
+			href={`/${route}/create`}
+			class="rounded-md bg-gray-800 px-4 py-2 text-white hover:bg-indigo-600"
+			>Create Contact</a>
+	</div>
+	<table class="mt-4 w-full table-auto rounded-lg bg-white shadow">
+		<thead>
+			<tr class="text-left">
+				<th class="p-4">Name</th>
+				<th class="p-4">Organization</th>
+				<th class="p-4">City</th>
+				<th class="p-4">Phone</th>
+				<th class="p-4" />
+			</tr>
+		</thead>
+		<tbody>
+			{#each contacts as contact}
+				<tr class="text-left focus-within:bg-gray-100 hover:bg-gray-100">
+					<td class="border-t p-4"
+						>{contact.firstName} {contact.lastName}</td>
+					<td class="border-t p-4">{contact.organization.name}</td>
+					<td class="border-t p-4">{contact.city}</td>
+					<td class="border-t p-4">{contact.phone}</td>
+					<td class="border-t">
+						<a
+							use:inertia
+							class="grid place-items-center p-4 hover:underline"
+							href={`/${route}/${contact.id}/edit`}>Edit</a>
+					</td>
+				</tr>
+			{/each}
+		</tbody>
+	</table>
+	<Pagination
+		{route}
+		{currentPage}
+		{perPage}
+		{total}
+		{pageNumbers}
+		{previousButtonDisabled}
+		{nextButtonDisabled} />
 </PageLayout>
--- Ping.Web/Controllers/ContactsController.cs
+++ Ping.Web/Controllers/ContactsController.cs
@@ -1,12 +1,48 @@
 ﻿using Inertia.NET.Extensions;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Ping.Core.Extensions.Pagination;
+using Ping.Data;
+using Ping.Data.Models;
 
 namespace Ping.Web.Controllers;
 
 public class ContactsController : Controller
 {
-    public IActionResult Index()
+    private readonly PingDbContext _db;
+
+    public ContactsController(PingDbContext db)
     {
-        return this.InertiaRender("Contacts/Index");
+        _db = db;
+    }
+
+    public IActionResult Index([FromQuery] PaginationQuery query)
+    {
+        var contactsPaginationResult = _db.Contacts
+            .Include(i => i.Organization)
+            .AsNoTracking()
+            .Select(s => new Contact
+            {
+                Id = s.Id,
+                FirstName = s.FirstName,
+                LastName = s.LastName,
+                Organization = new Organization
+                {
+                    Id = s.Organization.Id,
+                    Name = s.Organization.Name
+                },
+                City = s.City,
+                Phone = s.Phone
+            })
+            .Paginate<Contact>(
+                query,
+                w =>
+                    w.FirstName.ToLower().Contains(query.search.ToLower()) ||
+                    w.LastName.ToLower().Contains(query.search.ToLower()) ||
+                    w.Organization.Name.ToLower().Contains(query.search.ToLower()) ||
+                    w.City.ToLower().Contains(query.search.ToLower()) ||
+                    w.Phone.ToLower().Contains(query.search.ToLower())
+            );
+        return this.InertiaRender("Contacts/Index", contactsPaginationResult);
     }
 }
